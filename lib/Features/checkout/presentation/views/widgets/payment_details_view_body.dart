52de08f8c6aa7025008f588d9acd7086fd6d8039
Ali Dosoqi <106039568+Al-Dos024@users.noreply.github.com>
2024-09-15 15:59:55 +0300

    start with ui and finished card methods

---
+++ lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:checkout_payment/Features/checkout/presentation/views/widgets/payment_methods_list_view.dart';
+import 'package:flutter/material.dart';
+
+class PaymentDetailsViewBody extends StatelessWidget {
+  const PaymentDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [PaymentMethodsListView()],
+    );
+  }
+}
