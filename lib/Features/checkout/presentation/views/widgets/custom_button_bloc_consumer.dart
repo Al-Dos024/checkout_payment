414ffaa3043891455480e5b97150ca1f8292bcc4
Ali Dosoqi <106039568+Al-Dos024@users.noreply.github.com>
2024-09-19 01:02:19 +0300

    Done with add paypal gateway , done with course , next time add paymob

--- lib/Features/checkout/presentation/views/widgets/custom_button_bloc_consumer.dart
+++ lib/Features/checkout/presentation/views/widgets/custom_button_bloc_consumer.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:checkout_payment_ui/Features/checkout/presentation/manger/cubit/payment_cubit.dart';
 import 'package:checkout_payment_ui/Features/checkout/presentation/views/thank_you_view.dart';
 import 'package:checkout_payment_ui/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
 class CustomButtonBlocConsumer extends StatelessWidget {
   const CustomButtonBlocConsumer({
@@ -30,14 +33,85 @@ class CustomButtonBlocConsumer extends StatelessWidget {
       builder: (context, state) {
         return CustomButton(
             onTap: () {
-              PaymentIntentInputModel paymentIntentInputModel =
-                  PaymentIntentInputModel(
-                amount: '100',
-                currency: 'USD',
-                cusomerId: 'cus_Qrt79jQVeYMRxP',
-              );
-              BlocProvider.of<PaymentCubit>(context).makePayment(
-                  paymentIntentInputModel: paymentIntentInputModel);
+              // PaymentIntentInputModel paymentIntentInputModel =
+              //     PaymentIntentInputModel(
+              //   amount: '100',
+              //   currency: 'USD',
+              //   cusomerId: 'cus_Qrt79jQVeYMRxP',
+              // );
+              // BlocProvider.of<PaymentCubit>(context).makePayment(
+              //     paymentIntentInputModel: paymentIntentInputModel);
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => PaypalCheckoutView(
+                  sandboxMode: true,
+                  clientId:
+                      "AVxa2zfmuI8NM06YfLkon4W1jG4B28p9bnnulz4dWslxV62LTEJR8G3AKNKAfRsEZVRLmHQzlhLmOx_q",
+                  secretKey:
+                      "EDmq3ow7Yw5DagBRjsXL5xnHbx6OkEOtbBftDWQU5r8I0kQyYvM0mvLRr8-w3bp6bBufC_aRbUpkSG3C",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": '100',
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": '100',
+                          "shipping": '0',
+                          "shipping_discount": 0
+                        }
+                      },
+                      "description": "The payment transaction description.",
+                      // "payment_options": {
+                      //   "allowed_payment_method":
+                      //       "INSTANT_FUNDING_SOURCE"
+                      // },
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": '10',
+                            "currency": "USD"
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": '12',
+                            "currency": "USD"
+                          }
+                        ],
+
+                        // Optional
+                        //   "shipping_address": {
+                        //     "recipient_name": "Tharwat samy",
+                        //     "line1": "tharwat",
+                        //     "line2": "",
+                        //     "city": "tharwat",
+                        //     "country_code": "EG",
+                        //     "postal_code": "25025",
+                        //     "phone": "+00000000",
+                        //     "state": "ALex"
+                        //  },
+                      }
+                    }
+                  ],
+                  note: "Contact us for any questions on your order.",
+                  onSuccess: (Map params) async {
+                    log("onSuccess: $params");
+                    Navigator.of(context)
+                        .pushReplacement(MaterialPageRoute(builder: (context) {
+                      return const ThankYouView();
+                    }));
+                  },
+                  onError: (error) {
+                    log("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    print('cancelled:');
+                    Navigator.pop(context);
+                  },
+                ),
+              ));
             },
             isLoading: state is PaymentLoading ? true : false,
             text: 'Continue');
